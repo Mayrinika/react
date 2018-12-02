36d25282a06cc8f5e8b226f78b05998590b0edda
Mayrinika <mashuliy_u@mail.ru>
2018-12-02 21:29:36 +0500

    completed step2

--- tasks/src/1.2.Button/index.js
+++ tasks/src/1.2.Button/index.js
@@ -3,25 +3,25 @@ import ReactDom from 'react-dom';
 import './styles.css';
 
 /**
-    Напиши обработчик нажатия на кнопку.
-    При нажатии должно выводиться диалоговое окно с сообщением «Отправлено».
+ Напиши обработчик нажатия на кнопку.
+ При нажатии должно выводиться диалоговое окно с сообщением «Отправлено».
  */
 
 ReactDom.render(
-  <div className="root">
-    <div className="form">
-      <div style={{ marginBottom: '10px' }}>Нажми отправить</div>
-      <input type="button" className="button" value="Отправить" onClick={()=>alert('Отправлено')}/>
-    </div>
-  </div>,
-  document.getElementById('app')
+    <div className="root">
+        <div className="form">
+            <div style={{marginBottom: '10px'}}>Нажми отправить</div>
+            <input type="button" className="button" value="Отправить" onClick={() => alert('Отправлено')}/>
+        </div>
+    </div>,
+    document.getElementById('app')
 );
 
 /**
-    Подсказки:
-    - alert(msg) — создает простое диалоговое окно с сообщением msg
-    - Компоненты React, соответствующие HTML, поддерживают атрибуты onClick, onChange и т.д.
-      В них можно передать функцию-обработчик события.
-    - Стрелочные функции: (x, y) => { return x + y; } — «непроизводительный»,
-      но быстрый способ написать обработчик событий
+ Подсказки:
+ - alert(msg) — создает простое диалоговое окно с сообщением msg
+ - Компоненты React, соответствующие HTML, поддерживают атрибуты onClick, onChange и т.д.
+ В них можно передать функцию-обработчик события.
+ - Стрелочные функции: (x, y) => { return x + y; } — «непроизводительный»,
+ но быстрый способ написать обработчик событий
  */
